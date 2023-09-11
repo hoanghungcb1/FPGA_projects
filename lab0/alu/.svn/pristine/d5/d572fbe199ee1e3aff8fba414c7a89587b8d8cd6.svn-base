68f246209076201f1c5b25f916e6aad1a8695fa2
hoanghungcb1 <hoanghungcb10@gmail.com>
2023-09-11 15:46:13 +0700

    Remove lab0

--- lab0/alu/.svn/pristine/d5/d572fbe199ee1e3aff8fba414c7a89587b8d8cd6.svn-base
+++
@@ -1,4 +0,0 @@
-#define MAX_SIM 20
-
-void set_random(Vtop *dut, vluint64_t sim_unit) {
-}
--- lab0/alu/test/driver.cpp
+++
@@ -1,7 +0,0 @@
-#define MAX_SIM 10000
-
-void set_random(Vtop *dut, vluint64_t sim_unit) {
-	dut -> operand_a_i = 0x0000FFFF;
-	dut -> operand_b_i = 0x000000FF;
-	dut -> alu_op_i = rand() % 16;
-}
